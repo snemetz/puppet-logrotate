36471d1b158d8ffe6fd28c685fc3893b3f6321f8
Yuri Arabadji <yuri@fused.com>
2013-02-21 15:50:06 +0200

    sane, improved defaults for rhel

--- manifests/defaults/redhat.pp
+++ manifests/defaults/redhat.pp
@@ -10,18 +10,19 @@ class logrotate::defaults::redhat {
     create       => true,
     create_owner => 'root',
     create_group => 'utmp',
-    rotate       => 1,
   }
 
   logrotate::rule {
     'wtmp':
       path        => '/var/log/wtmp',
-      create_mode => '0664',
+      create_mode => '0660', # Why would a regular user want to know what host has root accessed the system from?
       missingok   => false,
-      minsize     => '1M';
+      minsize     => '1M',
+      rotate      => 3; # keep 3 months worth of login info
     'btmp':
       path        => '/var/log/btmp',
       create_mode => '0660',
-      minsize     => '1M';
+      minsize     => '1M',
+      rotate      => 1; # we usually don't care about failed logins in the past -- this info should be aggregated to central server and reacted upon
   }
 }
